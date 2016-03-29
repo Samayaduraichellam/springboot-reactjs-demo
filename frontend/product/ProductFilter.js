cf22abcdb5114d04333e60679f641d47fe6c8375
Benjamin Seber <seber@synyx.de>
2016-03-29 18:09:16 +0200

    productFilter keeps selected radio button in state now
    defaultChecked has no effect on rendering a component
    that is alive already

--- frontend/product/ProductFilter.js
+++ frontend/product/ProductFilter.js
@@ -4,23 +4,42 @@ import React from 'react'
 
 class ProductFilter extends React.Component {
 
+    constructor (props) {
+        super (props);
+        this.handleSubmit = this.handleSubmit.bind (this);
+        this.handleRadioButtonChange = this.handleRadioButtonChange.bind (this);
+        this.state = {
+            sortBy: props.sortBy
+        }
+    }
+
+    componentWillReceiveProps (nextProps) {
+        const { sortBy } = nextProps;
+        this.setState ({ sortBy })
+    }
+
     handleSubmit (event) {
                   event.preventDefault ();
         const sortBy = event.target.elements.sort.value;
         this.props.onSubmit ({ sortBy });
     }
 
+    handleRadioButtonChange (event) {
+        const sortBy = event.target.value;
+        this.setState ({ sortBy })
+    }
+
     render () {
 
         const {
             sortBy
-        } = this.props;
+        } = this.state;
 
         return (
             <form action="/" method="get" onSubmit={event => this.handleSubmit (event)}>
                 <b>sort by</b>
-                <FilterItem label="Name" value="name" checked={sortBy === 'name'}/>
-                <FilterItem label="Price" value="price" checked={sortBy === 'price'}/>
+                <FilterItem label="Name" value="name" checked={sortBy === 'name'} onChange={this.handleRadioButtonChange} />
+                <FilterItem label="Price" value="price" checked={sortBy === 'price'} onChange={this.handleRadioButtonChange} />
                 <button type="submit">submit</button>
             </form>
         );
@@ -30,10 +49,11 @@ class ProductFilter extends React.Component {
 const FilterItem = ({
     label,
     value,
-    checked
+    checked,
+    onChange
 }) => (
     <span>
-        <input type="radio" name="sort" value={value} id={`sort-${value}`} defaultChecked={checked}></input>
+        <input type="radio" name="sort" value={value} id={`sort-${value}`} checked={checked} onChange={onChange}></input>
         <label htmlFor={`sort-${value}`}>{label}</label>
     </span>
 );
